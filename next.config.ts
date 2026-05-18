21f4f7b883f1ea9530d4a9e515f4dddc8f69322c
Ibrahim <iboisa12@gmail.com>
2026-05-18 11:26:23 +0400

    Update Next.js configuration for image domains, upgrade next-intl to version 4.12.0, and add middleware for internationalization routing.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['admin.fleurgarden.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'admin.fleurgarden.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
